8fe99917773741b0886a3cf4f8f2e4c74066e038
Sachin Jangra <androxpm@gmail.com>
2015-12-09 18:15:33 +0530

    Adding files for new type of question i.e Content and tips

---
+++ app/src/main/java/com/sigrideducation/englishlearning/model/question/ContentTipQuestion.java
@@ -0,0 +1,33 @@
+package com.sigrideducation.englishlearning.model.question;
+
+import android.os.Parcel;
+
+public final class ContentTipQuestion extends Question<String> {
+
+
+    public ContentTipQuestion(String question, String answer, boolean solved) {
+        super(question, answer, solved);
+    }
+
+    @SuppressWarnings("unused")
+    public ContentTipQuestion(Parcel in) {
+        super(in);
+        setAnswer(in.readString());
+    }
+
+    @Override
+    public String getStringAnswer() {
+        return getAnswer();
+    }
+
+    @Override
+    public QuestionType getType() {
+        return QuestionType.CONTENT_TIP;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeString(getAnswer());
+    }
+}
---
+++ app/src/main/java/com/sigrideducation/englishlearning/widget/question/ContentTipQuestionView.java
@@ -0,0 +1,68 @@
+package com.sigrideducation.englishlearning.widget.question;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.sigrideducation.englishlearning.R;
+import com.sigrideducation.englishlearning.model.Lesson;
+import com.sigrideducation.englishlearning.model.question.ContentTipQuestion;
+
+@SuppressLint("ViewConstructor")
+public class ContentTipQuestionView extends AbsQuestionView<ContentTipQuestion> {
+
+    private static final String KEY_ANSWER = "ANSWER";
+
+    private TextView mTextData;
+
+
+    public ContentTipQuestionView(Context context, Lesson lesson, ContentTipQuestion question,boolean show) {
+        super(context, lesson, question,show);
+    }
+
+    @Override
+    protected View createQuestionContentView() {
+        final Context context = getContext();
+        LinearLayout container = (LinearLayout) getLayoutInflater().inflate(R.layout.question_content_tip, this, false);
+        mTextData =(TextView) container.findViewById(R.id.text_data);
+
+        container.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                allowAnswer(true);
+                return true;
+            }
+        });
+        mTextData.setText(getQuestion().getAnswer());
+        mTextData.setTextSize(30);
+        return container;
+    }
+
+
+
+    @Override
+    protected boolean isAnswerCorrect() {
+
+        return true;
+    }
+
+    @Override
+    public Bundle getUserInput() {
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_ANSWER,"data");
+        return bundle;
+    }
+
+    @Override
+    public void setUserInput(Bundle savedInput) {
+        if (savedInput == null) {
+            return;
+        }
+
+    }
+
+}
