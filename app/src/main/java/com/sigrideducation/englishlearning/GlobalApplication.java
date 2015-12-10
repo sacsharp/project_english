e8d0af28146cbff4c5f348f07b0ac613d64c0ea9
Sachin Jangra <androxpm@gmail.com>
2015-12-10 17:59:17 +0530

    Adding Global Application file.

---
+++ app/src/main/java/com/sigrideducation/englishlearning/GlobalApplication.java
@@ -0,0 +1,37 @@
+package com.sigrideducation.englishlearning;
+
+import android.app.Application;
+
+/**
+ * Created by Sachin on 12/10/2015.
+ */
+public class GlobalApplication extends Application {
+    private boolean submitAnswerGuideShown=false;
+    private boolean trueGuideShown=false;
+    private boolean falseGuideShown=false;
+
+
+    public boolean isSubmitAnswerGuideShown() {
+        return submitAnswerGuideShown;
+    }
+
+    public void setSubmitAnswerGuideShown(boolean submitAnswerGuideShown) {
+        this.submitAnswerGuideShown = submitAnswerGuideShown;
+    }
+
+    public boolean isTrueGuideShown() {
+        return trueGuideShown;
+    }
+
+    public void setTrueGuideShown(boolean trueGuideShown) {
+        this.trueGuideShown = trueGuideShown;
+    }
+
+    public boolean isFalseGuideShown() {
+        return falseGuideShown;
+    }
+
+    public void setFalseGuideShown(boolean falseGuideShown) {
+        this.falseGuideShown = falseGuideShown;
+    }
+}
